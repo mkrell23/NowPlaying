f320c96c6f55539c9516196374a0704fb443a5f6
mkrell23 <mkrell23@gmail.com>
2020-06-13 13:35:02 -0400

    allows user selection of search

--- Program.cs
+++ Program.cs
@@ -7,27 +7,52 @@ namespace NowPlaying
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-           var result = WebInteraction.SearchUtelly("bojack");
-           var content = JsonConvert.DeserializeObject<String>(result.Content);
-           var movie = Movie.FromJson(result.Content);
+
+            // UserInteraction.DisplayWelcome();
+            
+            // var selection = Console.ReadKey();
+            // if (selection.Key == ConsoleKey.Escape)
+            // {
+            //     return 0;
+            // }
+            // else
+            // {      
+            //     Console.Clear();    
+            //     movieChoice = UserInteraction.MainMenu();
+            // }
+
+            UserInteraction.MainMenu();
+            var movieChoice = Console.ReadLine();
+
+            
+            // UserInteraction.MainMenu();
+            // string selection = Console.ReadLine();
+            // Console.WriteLine($"You picked {selection}");
+            
+            var result = WebInteraction.SearchUtelly(movieChoice);
+            var content = JsonConvert.DeserializeObject(result.Content);
+
+            WebInteraction.PrintUtellyKey();
+            Console.WriteLine("Hello World!");
+
+            //    var movie = Movie.FromJson(result.Content);
 
          
      
-        //    Movie bojack = WebInteraction.ReadMovieInfo(movie2.);
-        //    var bojack = Movie.DeserializeMovie(content);
+        // //    Movie bojack = WebInteraction.ReadMovieInfo(movie.);
+        // //    var bojack = Movie.DeserializeMovie(content);
 
-        //    JToken token = JToken.Parse(result.Content);
-        //    JObject json = JObject.Parse( token);
+        // //    JToken token = JToken.Parse(result.Content);
+        // //    JObject json = JObject.Parse( token);
              
             
-            WebInteraction.ReadMovieInfo(content);
+        //     WebInteraction.ReadMovieInfo(content);
 
-          WebInteraction.SaveMovieToFile(content, "bojack.json") ; 
-            WebInteraction.PrintUtellyKey();
-            Console.WriteLine("Hello World!");
-  
+        //   WebInteraction.SaveMovieToFile(content, "bojack.json") ; 
+
+            return 0;
         }
     }
 }
--- UserInteraction.cs
+++ UserInteraction.cs
@@ -1,11 +1,56 @@
+using System;
+
 namespace NowPlaying
 {
     //This will hold the code that the user interface is built from.
     class UserInteraction
     {
-        //Needs templates for:
-            // welcome messages and prompts, 
-            // recieving user input, 
-            // displaying the results
+        public static void DisplayWelcome()
+        {
+            Console.WriteLine(@"
+.__   __.   ______   ____    __    ____ 
+|  \ |  |  /  __  \  \   \  /  \  /   / 
+|   \|  | |  |  |  |  \   \/    \/   /  
+|  . `  | |  |  |  |   \            /   
+|  |\   | |  `--'  |    \    /\    /    
+|__| \__|  \______/      \__/  \__/     
+                                        
+.______    __          ___   ____    ____  __  .__   __.   _______ 
+|   _  \  |  |        /   \  \   \  /   / |  | |  \ |  |  /  _____|
+|  |_)  | |  |       /  ^  \  \   \/   /  |  | |   \|  | |  |  __  
+|   ___/  |  |      /  /_\  \  \_    _/   |  | |  . `  | |  | |_ | 
+|  |      |  `----./  _____  \   |  |     |  | |  |\   | |  |__| | 
+| _|      |_______/__/     \__\  |__|     |__| |__| \__|  \______| 
+__________________________________________________________________
+
+Find what streaming provider is serving a movie!
+
+Press any key to start
+Press the ESC key at any tme to exit");
+
+        }
+
+        public static void MainMenu()
+        {
+            Console.WriteLine(@"
+.__   __.   ______   ____    __    ____ 
+|  \ |  |  /  __  \  \   \  /  \  /   / 
+|   \|  | |  |  |  |  \   \/    \/   /  
+|  . `  | |  |  |  |   \            /   
+|  |\   | |  `--'  |    \    /\    /    
+|__| \__|  \______/      \__/  \__/     
+                                        
+.______    __          ___   ____    ____  __  .__   __.   _______ 
+|   _  \  |  |        /   \  \   \  /   / |  | |  \ |  |  /  _____|
+|  |_)  | |  |       /  ^  \  \   \/   /  |  | |   \|  | |  |  __  
+|   ___/  |  |      /  /_\  \  \_    _/   |  | |  . `  | |  | |_ | 
+|  |      |  `----./  _____  \   |  |     |  | |  |\   | |  |__| | 
+| _|      |_______/__/     \__\  |__|     |__| |__| \__|  \______| 
+__________________________________________________________________
+
+Type the name of the movie you want to search for:
+");
+        }
+
     }
 }
